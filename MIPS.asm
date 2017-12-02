a21e43c78e4bee8f4f948b4103ec2194047cb3b1
cicelybeckford <cicelybeckford@users.noreply.github.com>
2017-12-01 23:46:27 -0500

    Added header comments, modified register and stack usage, changed how "," are printed

--- MIPS.asm
+++ MIPS.asm
@@ -1,53 +1,81 @@
-#PROGRAM: CONVERT A STRING WITH MULTIPLE HEXADECIMAL VALUES
+#PROGRAM: CONVERT A STRING WITH MULTIPLE HEXADECIMAL VALUES TO THEIR DECIMAL VALUES
 
 .data  
-	output2: .asciiz ","
+	newline: .asciiz "\n"
 	invalid: .asciiz "NaN"
 	toolarge: .asciiz "too large"
 	buffer:  .space 1001
-	substring: .space 1001
+	substring: .space 1001    #assuming that the user input can be 1 hexadecimal with 1000 characters(including spaces)
 .text
-#GETS USER INPUT AND CALLS THE SUBPROGRAMS 
+###########################################################main
+#GETS USER INPUT AND CALLS SUBPROGRAM_2 AND SUBPROGRAM_3
+#
+# Arg registers used: $a0, $a1
+# Sve registers used: $s0, $s6, $s7
+# Tmp registers used: $t2
+# Pointers used: $sp
+#
+# Pre: none
+# Post: none
+# Returns: none
+#
+# Called by: none
+# Calls: subprogram_2, subprogram_3  
 main: 
 	 			li $v0, 8                  #system call code for reading string = 8
 				la $a0, buffer             #load byte space into address
 				li $a1, 1001               #allot the byte space for hexadecimal
 				syscall
 				
-				addiu $a1, $a0, 0          #move address of hexadecimal into $a1
+				addi $a1, $a0, 0           #move address of hexadecimal into $a1 then into $s5
+				addi $s7, $a1, 0
 				la $s6, substring          #load byte space into address for substring
 				j ILOOP                    #jumps to inner loop
 				
 	OLOOP:		la $s6, substring          
 			    li $t2, 0                  #set $t2's value to 0
-			    addi $a1, $a1, 1           #increment address pointer
-	ILOOP:		lb $s0, ($a1)              #get first byte of string and store in $s0
+			    addi $s7, $s7, 1           #increment address pointer
+	ILOOP:		lb $s0, ($s7)              #get first byte of string and store in $s0
 	            beq $s0, 10, STOP          #if the character is the DLE character stop the inner loop
 				beq $s0, 44, STOP          #stops the inner loop if the character is a comma
 	            sb $s0, ($s6)              #add the character to the substring
 	            addi $t2, $t2, 1           #increment the loop counter
-		        addi $a1, $a1, 1           #increment the address pointer
+		        addi $s7, $s7, 1           #increment the address pointer
 		        addi $s6, $s6, 1           #increment the substring's address pointer
 		        j ILOOP                    #jump to top of inner loop
 				
 	STOP:	    sub $s6, $s6, $t2          #reinitalize the substring's address pointer
-	            addi $sp, $sp, -12         #reserve space in stack 
-				sw $s6, 0($sp)			   #store substring in stack
-				sw $t2, 4($sp)             #store the substring's length in stack 
+	            addi $sp, $sp, -4          #reserve space in stack for decimal 
+				add $a0, $s6, $zero		   #pass substring as a parameter
+				add $a1, $t2, $zero        #pass substring's length as a parameter
 				jal subprogram_2           #jump to subprogram_2 to convert the string
 				jal subprogram_3           #jump to subprogram_3 to print results of conversion
-				addi $sp, $sp, 12          #cancel space in stack
+				addi $sp, $sp, 4           #cancel space in stack
 				beq $s0, 10, END           #if the last character was the DLE character end the program
 				
-				la $a0, output2            #address of string to print
-				li $v0, 4		           #system call code for printing string = 4
+				la $a0, 44            	   #load character to print
+				li $v0, 11		           #system call code for printing character = 11
 				syscall
 				j OLOOP                    #jump to top of outer loop
 				
-	END:        li $v0, 10                 #terminate program 
+	END:        la $a0, newline            #address of string to print
+				li $v0, 4		           #system call code for printing string = 4
+				syscall
+				li $v0, 10                 #terminate program 
 			    syscall                    #and exit
-				
-#SUBPROGRAM TO CONVERT CHARACTERS TO THEIR DECIMAL VALUES	   
+
+####################################################################subprogram_1				
+#SUBPROGRAM TO CONVERT A HEXADECIMAL CHARACTER TO ITS DECIMAL VALUE 
+#
+# Arg registers used: $a0
+# Tmp registers used: $t0, $t1, $t2, $t3, $t4, $t5, $t6
+#
+# Pre: $a0 contains character to convert
+# Post: $v0 contains the return value
+# Returns: the value of $a0 as a decimal
+#
+# Called by: subprogram_2
+# Calls: none   
 subprogram_1:
 				li $t1, '0'                #holds character '0'
 				li $t2, '9' 		       #holds character '9'
@@ -77,33 +105,46 @@ subprogram_1:
 				jr $ra 
 	INVALID:    addi $v0, $zero, -3        #return -3 if the character is invalid
 				jr $ra  				
-				
-#SUBPROGRAM TO CONVERT HEXADECIMAL STRING TO DECIMAL INTEGERS		
+
+#########################################################subprogram_2				
+#SUBPROGRAM TO CONVERT HEXADECIMAL STRINGS TO DECIMAL INTEGERS	
+#
+# Arg registers used: $a0, $a1
+# Tmp registers used: $t0, $t2, $t3, $t4, $t7, $t8, $t9
+# Sve registers used: $s1, $s2, $s4, $s5
+# Pointers used: $sp
+#
+# Pre: $a0 contains substring and $a1 contains substring length
+# Post: 0($sp) contains decimal value
+# Returns: the decimal value of the subtring passed in $a0
+#
+# Called by: main
+# Calls: subprogram_1	
 subprogram_2:	
-				lw $a2, 0($sp)               #read substring from stack
-				lw $t7, 4($sp)               #read subtstring length from stack
+				add $s5, $a0, $zero			 #copy substring from $a0
+				add $t7, $a1, $zero          #copy string length from $a1
 				li $s2, 1				     #initialize count to 0
 				li $s1, 0		             #initialize decimal to 0
 				li $t0, 0                    #set $t0's value to 0
 				beq $t7, 0, NOTVALID         #if the substring's length is 0 jump to INVALID procedure
 				sub $t9, $t7, 1              #set $t9's value to 1 less than the substring's length
-				add $a2, $a2, $t9            #increment the substring's address pointer by the value of $t9
+				add $s5, $s5, $t9            #increment the substring's address pointer by the value of $t9
 		
-    #REMOVES SPACES AT THE END OF THE STRING		
-    CHECK:	    lb $s4, ($a2)				#load character into $s4
+    #if there are spaces at the end of the string decrement the string length value		
+    CHECK:	    lb $s4, ($s5)				#load character into $s4
 				beq $s4, 32, SKIPTAB       	#if the character is a space jump to SKIPTAB
 				bne $s4, 9, ENDCHECK        #if the character is not a space or tab exit CHECK procedure
 	SKIPTAB:	sub $t7, $t7, 1             #decrement string length count
 				sub $t9, $t9, 1             #as well as $t9's value
 				beq $t7, 0, NOTVALID        #if the length of string is equal to 0 jump to NOTVALID
-				sub $a2, $a2, 1             #decrement the string pointer
+				sub $s5, $s5, 1             #decrement the string pointer
 				j CHECK                     #jump to top of CHECK procedure
-	ENDCHECK:	sub $a2, $a2, $t9           #decrement the pointer to the beginning of the string
+	ENDCHECK:	sub $s5, $s5, $t9           #decrement the pointer to the beginning of the string
 				li $t9, 1                   #set $t9's value to 1
 				li $s4, 0                   #set $s4's value to 0
 				
     WHILELOOP:  move $t0, $s4               #move last read character into $t0 register
-				lb $s4, ($a2)               #load the next character into $t4
+				lb $s4, ($s5)               #load the next character into $s4
 				bgt $t9, $t7, LARGE         #exit loop if $t9 is equal to the length of the substring
 				beq $t0, 32, DO             #if the character in $t0(previous character) is a space
 				beq $t0, 9, DO              #or if the character is a tab
@@ -111,10 +152,10 @@ subprogram_2:
 	DO:         beq $s4, 32, SKIP           #or if $t0 and $s4 (current character) are spaces 
 				beq $s4, 9, SKIP			#or tabs jump to SKIP
     NOSPACE:    addi $sp, $sp, -4           #reserve space in stack 
-                sw $ra, 12($sp)				#store return address in stack
-				add $a0, $zero, $s4         #store the character in $a0
+                sw $ra, 4($sp)				#store return address in stack
+				add $a0, $zero, $s4         #store the character in $a0 as a parameter
 				jal subprogram_1            #jump to subprogram 1
-				lw $ra, 12($sp)             #move the old return address from stack back into $ra
+				lw $ra, 4($sp)              #move the old return address from stack back into $ra
 				addi $sp, $sp, 4            #cancel space in stack
 				add $v1, $v0, $zero         #move the results into $v1
 				beq $v1, -3, NOTVALID       #if the returned value is -3 then the character is invalid
@@ -141,7 +182,7 @@ subprogram_2:
 				addi $s2, $s2, 1           #increment the count
 				
 	SKIP:		addi $t9, $t9, 1           #increment $t9
-				addi $a2, $a2, 1           #increment the string pointer
+				addi $s5, $s5, 1           #increment the string pointer
 				j WHILELOOP                #return to the top of the loop
 	
 	NOTVALID:   li $s1, -3                 #if the string is NaN set the decimal's value to -3
@@ -150,12 +191,25 @@ subprogram_2:
 	LARGE:      ble $s2, 9, EXIT           #if the string length without spaces is greater than 9  
 				li $s1, -2                 #set the decimal's value to -2
 				
-	EXIT:	    sw $s1, 8($sp)             #put return value in the stack
+	EXIT:	    sw $s1, 0($sp)             #put return value in the stack
 	            jr $ra					   #exit subprogram
 				                 
- 
-#SUBPROGRAM TO PRINT THE CONVERTED HEXADECIMAL STRINGS
-subprogram_3:   lw $s1, 8($sp)               #get decimal from stack
+
+##############################################subprogram_3 
+#SUBPROGRAM TO PRINT THE CONVERTED DECIMAL VALUES
+#
+# Arg registers used: $a0 
+# Tmp registers used: $t0
+# Sve registers used: $s1
+# Pointers used: $sp
+#
+# Pre: 0($sp)contains decimal value to be outputted
+# Post: decimal value is outputted
+# Returns: none
+#
+# Called by: main
+# Calls: none   
+subprogram_3:   lw $s1, 0($sp)               #get decimal from stack
 				bge $s1, $zero, POSITIVE     #if the decimal value is positive jump to POSITIVE label 
                 beq $s1, -3, NAN             #if the value is -3 jump to output for invalid strings
 				beq $s1, -2, TOOLARGE        #if the value is -2 jump to output for hexadecimals that are too large
